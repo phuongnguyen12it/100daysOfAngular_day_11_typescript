0b3011b1ce97de5d836b725fb58ecd72926f4c47
phuongnguyen12it <phuongnguyen12it@gmail.com>
2022-07-26 09:17:34 +0700

    creation poerator in RxJS

--- index.ts
+++ index.ts
@@ -45,9 +45,8 @@ timer(1000, 1000); //.subscribe(observer);
 throwError('error').subscribe(observer);
 
 //defer muon retry mot apicall hoan toan moi
+//const random$ = Math.random()// neu ko dung defer thi cac gia tri ramdom goi lai phia duoi se luon la mot gia trij giong nhau, thay vi do ta dung nhu phia duoi de ham random luon dc chay moi lan goi
 const random$ = defer(() => of(Math.random()));
 random$.subscribe(observer);
 random$.subscribe(observer);
 random$.subscribe(observer);
-
-defer(() => {});
