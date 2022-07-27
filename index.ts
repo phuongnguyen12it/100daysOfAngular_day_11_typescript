6c30c1a349b346d91be579bf503c8bd835106428
phuongnguyen12it <phuongnguyen12it@gmail.com>
2022-07-27 16:00:37 +0700

    1 file modified

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import { fromEvent, interval, of } from 'rxjs';
-import { map, mergeAll, take } from 'rxjs/operators';
+import { fromEvent, interval, merge, of, partition } from 'rxjs';
+import { map, mergeAll, take, tap } from 'rxjs/operators';
 
 //Observable<number>
 //Observable<string>
@@ -27,3 +27,17 @@ const hoo = fromEvent(document, 'click') //Outer Observable (parent Observable)
 //swichMap() = switchAll() + map() // dg subscribe vao cai dau tien, ma co observebale thu 2 cancle cai dau tien subscribe vao cai thu 2, luon chi co 1 observable vao 1 thoi diem => thich họp cho cancel xhr request
 //concatMap() = concatAll() + map() subscribe theo thứ tự, complete cai 1 mới sang cái 2 và cứ thế tới cái cuối cùng
 //exhaustMap() //trong lúc dg subscrible mà có obsevebal thì đều bị cancle,
+
+const interval$ = interval(1000);
+const [even$, odd$] = partition(interval$, (val) => val % 2 === 0);
+
+// merge(even$.pipe(map((val) => `I am even: ${val}`))); //.subscribe(console.log);
+
+//tap
+even$
+  .pipe(
+    tap((val) => console.log('before map', val)),
+    map((val) => `I am even: ${val * 2}`),
+    tap((val) => console.log('after map', val)),
+    //take(5)
+  );//.subscribe(console.log);
